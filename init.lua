72bfa235fe5fbfc7894210de92c80b48e36b15b9
mokbel karam <karammokbel@gmail.com>
2026-05-01 22:56:08 -0600

    added cpp, csv, python, and zig to nvim-treesitter parsers

--- init.lua
+++ init.lua
@@ -892,7 +892,7 @@ require('lazy').setup({
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter-intro`
     config = function()
       -- ensure basic parser are installed
-      local parsers = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' }
+      local parsers = { 'bash', 'c', 'cpp', 'csv', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'python', 'vim', 'vimdoc', 'zig' }
       require('nvim-treesitter').install(parsers)
 
       ---@param buf integer
