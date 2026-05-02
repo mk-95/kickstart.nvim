3e0619a33524755addab6f1613fe5ba2fc99b077
mokbel karam <karammokbel@gmail.com>
2026-05-02 15:14:46 -0600

    added lsp servers to ensure installing them with Mason.

--- init.lua
+++ init.lua
@@ -661,6 +661,12 @@ require('lazy').setup({
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
         -- You can add other tools here that you want Mason to install
+        'cmakelang',
+        'clangd',
+        'clang-format',
+        'autopep8',
+        'autoflake',
+        'zls'
       })
 
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
